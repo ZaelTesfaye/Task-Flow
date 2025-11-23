5ce132d343c3fff4572de5166e4d2e5b3146562e
Zeal.T <teszael14@gmail.com>
2025-11-23 20:55:40 +0300

    fix: update route paths to include '/admin' prefix for consistency

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/admin">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
