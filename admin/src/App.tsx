9299c3328d94c04306c05261c11e61d0cf2f4676
Zeal.T <teszael14@gmail.com>
2025-11-23 20:53:24 +0300

    refactor: remove unnecessary BrowserRouter wrapping in App component

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -7,7 +7,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Header from "./components/Header";
 
 function Layout() {
-  // This layout wraps protected pages with a header
+  // Wraps protected pages with the shared header
   return (
     <>
       <Header />
@@ -19,23 +19,21 @@ function Layout() {
 function App() {
   return (
     <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
-      <BrowserRouter basename="/admin">
-        <Toaster position="top-right" />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-          {/* Protected routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route element={<Layout />}>
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Route>
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
+        </Route>
 
-          {/* 404 route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+        {/* 404 route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
