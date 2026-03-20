68af9d3ae04b902a4cf27210f60e4362d42d69f3
Zeal.T <teszael14@gmail.com>
2026-03-20 18:36:09 +0300

    updated config

--- client/src/app/api/health/route.ts
+++ client/src/app/api/health/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic"; // Disable edge caching
+export const dynamic = "force-static";
 
 export async function GET() {
   return NextResponse.json(
